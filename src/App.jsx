52980a981b127384f2ede552b55eb16a4ea2b043
Ashish Lamichaney <124287828+A5shi5h@users.noreply.github.com>
2024-05-26 18:40:37 +0530

    displaying items

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,30 @@ import './App.css'
 function App() {
 
   const[total , setTotal] = useState(0)
+  const[item , setItem] = useState("")
+  const[list , setList] = useState([])
   let[newTotal , setNewTotal] = useState(0)
   
+  let nextId = 0
 
   const calculateExpense = () => {
     newTotal = Number(newTotal) + Number(total)
     setNewTotal(newTotal)
-    console.log(newTotal)
+    setItem(item)
+    list.push(item)
+    setList([...list , {id: nextId++ , name: item}])
+    console.log(list)
   }
 
   return (
     <>
       <div className='h-96 w-full bg-red-500 rounded-md'>
-        <h1 className='font-semibold text-2xl p-4'>My Expense Tracker</h1>
+        <h1 className='font-semibold text-4xl p-4'>My Expense Tracker</h1>
         <label>Item name : </label>
          <input type='text' placeholder='enter the item name' 
          className='p-3 mt-4 rounded-lg bg-slate-200'
+         value={item}
+         onChange={e => setItem(e.target.value)}
          />
          <br />
          <label>Item price : </label>
@@ -35,8 +43,19 @@ function App() {
          onClick={calculateExpense}
          >Add</button>
          <br />
-         <h1 className=''>My Total Expense</h1>
-         <p>{newTotal}</p>
+         <h1 className=''>Money Spent</h1>
+         <p>Rs.{newTotal}</p>
+      </div>
+      <div className='w-full h-auto bg-slate-300 mt-2'>
+          <ul className='mt-4 pb-4'>
+            {list.map(items => (
+              <li key={items.id}>
+                <div>
+                  <p>{items.name}</p>
+                </div>
+              </li>
+            ))}
+          </ul>
       </div>
     </>
   )
